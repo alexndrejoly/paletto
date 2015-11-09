7f09f6d6271ddcad4bbbccab7b6af4713446d84e
Alexandre Joly <Alexandre Joly>
2015-11-09 15:57:36 +0100

    Archivage du test pour la première histoire

--- src/Engine.js
+++ src/Engine.js
@@ -3,6 +3,13 @@
 var Engine = function () {
 
 // private attributes and methods
+    var _board = new Array(36);
+
+    this.initBoard = function() {
+
+
+        return false;
+    };
 
 // public methods
 };
--- test/test.js
+++ test/test.js
@@ -3,4 +3,6 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var engine = new Engine();
+    assertEquals(true, engine.initBoard());
 };
